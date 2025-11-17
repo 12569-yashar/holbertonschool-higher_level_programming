d47dc740247f80cc0f3864194b564a799796d4b0
Your Name <you@example.com>
2025-11-17 15:38:41 +0400

    salam

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]          # Fayl adını çıxırıq
+    count = len(args)            # Arqumentlərin sayı
+
+    if count == 0:
+        print("0 argument.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print(f"{count} arguments:")
+
+    for i, arg in enumerate(args, 1):
+        print(f"{i}: {arg}")
