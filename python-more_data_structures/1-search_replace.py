0976eb2302317ca830bac32542e71ac16ae6ed4e
Your Name <you@example.com>
2025-11-19 14:11:08 +0400

    salam

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-for i in my_list:
-    if  i==search:
-	i=replace
-return my_list
+    new_list = []
+    for i in my_list:
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(i)
+    return new_list
