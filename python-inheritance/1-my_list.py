974ed10a123708fc5106b1f2ce15c5cef5fa4a16
root <you@example.com>
2025-11-26 10:38:36 +0000

    sagray

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-"""MyList class definition"""
+"""
+This module has a class MyList that inherits from
+list. Has a public instance method print_sorted that
+prints the list in ascending order
+
+"""
 
 
 class MyList(list):
-    """Custom list class with a method to print sorted list"""
+    """MyList class subclass of list
+    Attributes:
+    print_sorted: prints the list in a sorted fashion
+    """
 
     def print_sorted(self):
-        """Prints the list in sorted order"""
-        self.sort()
-        print(self)
+        """print_sorted method prints list sorted"""
+        print(sorted(self))
