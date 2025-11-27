c9d6f91e770c6c8b2b7e69c6aa811e98374b2da6
Your Name <you@example.com>
2025-11-27 17:16:20 +0400

    commit

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """salam"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """salam"""
-    with open(filename, 'a', encoding="utf-8") as f:
+    with open(filename, 'a', encoding="utf-8")as f:
         return (f.write(text))
