607606869038d3e68b09846a9cf9a00ca64963f2
Your Name <you@example.com>
2025-11-27 16:59:35 +0400

    commit

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,5 +5,4 @@
 def write_file(filename="", text=""):
     """dasad"""
     with open(filename, encoding="utf-8") as f:
-        f.write("Holberton School is so cool!\n")
-
+        return f.write("Holberton School is so cool!\n")
