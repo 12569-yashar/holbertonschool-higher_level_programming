0a26f385dc7df2d4e68da9a2adffc2b150167226
Your Name <you@example.com>
2025-11-27 17:12:41 +0400

    commit

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""salam"""
+
+
+def append_write(filename="", text=""):
+    """salam"""
+     with open(filename, "r", encoding="utf-8") as f:
+    f = open('workfile', 'w', encoding="utf-8")
