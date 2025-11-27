5f75296747236d3173d6cb358f2003c17835203a
Your Name <you@example.com>
2025-11-27 14:40:16 +0400

    commit

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-"""salam"""
+"""
+Method module
+"""
 
 
 def inherits_from(obj, a_class):
-    """salam"""
+    """check if object is an instance of a class
+    args:
+        obj: object to check
+        a_class: class to check
+    returns:
+        True or False
+    """
+
     if type(obj) == a_class:
         return False
     return issubclass(type(obj), a_class)
