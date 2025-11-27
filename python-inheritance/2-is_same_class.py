20d77c6fd93d2ced026bec54a678ca63196cc99a
Your Name <you@example.com>
2025-11-27 16:32:13 +0400

    commit

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""
-This module contains the function is_same_class
-"""
+"""2. Exact same object"""
 
 
 def is_same_class(obj, a_class):
-    """return true if obj is the exact class a_class, otherwise false"""
-    return (type(obj) == a_class)
+    """
+    Returns True if obj is exactly an instance of a_class.
+    """
+    return type(obj) is a_class
