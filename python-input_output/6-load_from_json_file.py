f0b6d82a318d9a9f324248836ec7d8584e14345d
Your Name <you@example.com>
2025-11-27 17:51:52 +0400

    commit

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,7 +5,7 @@ import json
 
 
 def load_from_json_file(filename):
-    """salam"""
-    with open(filename, "r", encoding="utf-8") as f:
-        return json.load(f)
+    """salam """
 
+    with open(filename, mode="r", encoding='utf-8') as my_file:
+        return json.load(my_file)
