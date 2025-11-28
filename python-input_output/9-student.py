3ed411a209e8e28bd09695ee4b939a5b638bc5ab
root <you@example.com>
2025-11-28 13:08:49 +0000

    sagray

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,11 +4,13 @@
 
 class Student:
     """salam"""
+
     def __init__(self, first_name, last_name, age):
-        self.first_name=first_name
-        self.last_name=last_name
-        self.age=age
-        
-    def to_json(self):
         """salam"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ salam"""
         return self.__dict__.copy()
