4221c1372cd8ae04b1269458cf7939f4cab8d6ea
root <you@example.com>
2025-11-29 15:22:14 +0000

    sagray

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,5 @@
 -- whattttttt
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM hbtn_0d_tvshows
-ORDER BY tv_shows.title, tv_show_genres.genre_id DESC;
+FROM tv_shows
+JOIN tv_show_genres ON  tv_shows.id=tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC , tv_show_genres.genre_id ASC;
