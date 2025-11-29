a0e8503e9d53c2a943c845675ccdc3ed3f2462cf
root <you@example.com>
2025-11-29 13:36:41 +0000

    sagray

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
 -- salam
-GRANT ALL PRIVILEGES ON hbtn_0c_0.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON hbtn_0c_0.* TO 'user_0d_2'@'localhost';
-
-FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
